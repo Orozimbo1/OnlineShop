f49420579aed05f596de633d8a77cfdbb17780b6
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-18 23:58:10 -0300

    url banco json-server versel

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://fake-api-jsonserver-sigma.vercel.app/',
     // baseURL: 'https://onlineshop-1.herokuapp.com',
 });
